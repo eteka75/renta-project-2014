174c18dae418809ca3e658124f3f972c68453262
ETEKA Wilfried <etekawilfried@gmail.com>
2024-01-25 00:00:21 +0100

    Save 2 Cookie 2

--- app/Http/Middleware/DeleteLocationCookie.php
+++ app/Http/Middleware/DeleteLocationCookie.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 
 class DeleteLocationCookie
@@ -15,6 +16,19 @@ class DeleteLocationCookie
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $reservationCode = $request->input('r_code', $request->cookie('r_code'));
+
+        if ($reservationCode) {
+            $response = new Response();
+           $this->deleteReservationCodeCookie($response);
+        
+            // You can also return a response with the cookie deleted
+            //return $response;
+        }
         return $next($request);
+    }
+    public function deleteReservationCodeCookie(Response $response)
+    {
+        return $response->cookie(Cookie::forget('r_code'));
     }
 }
