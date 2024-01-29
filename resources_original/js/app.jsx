8e5651a8013c5ab58aaf1aa6753cca0cad6dad48
ETEKA Wilfried <etekawilfried@gmail.com>
2024-01-29 02:34:05 +0100

    App:Home edit for Loading

--- resources_original/js/app.jsx
+++ resources_original/js/app.jsx
@@ -1,7 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createRoot } from 'react-dom/client';
+/*import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
@@ -9,8 +9,8 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
+   // resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/* *_*.jsx')),
+   /* setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
@@ -18,4 +18,45 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+});*/
+//import './bootstrap';
+import '../css/app.css';
+import { useState } from 'react';  // Ajout de useState
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = import.meta.env.VITE_APP_NAME || ' Rental Car Services';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        // Ajout d'un état local pour gérer le chargement
+        const [loading, setLoading] = useState(true);
+
+        // Mettez à jour l'état lorsque le chargement est terminé
+        const handleLoadingComplete = () => {
+            setLoading(false);
+        };
+
+        root.render(
+            <>
+                {loading && (
+                    <div style={{ position: 'fixed', top: '50%', left: '50%', background:'red', transform: 'translate(-50%, -50%)' }}>
+                        {/* Vous pouvez remplacer cette icône par la vôtre */}
+                        <div className="loader">Chargement...</div>
+                    </div>
+                )}
+                <App {...props} onLoadingComplete={handleLoadingComplete} />
+            </>
+        );
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
+
